67245a0cc8cd71d3da52875f4a4b60a032c604b6
JezisVedToJeMojzis <mojzissamuel@gmail.com>
2023-05-01 19:57:38 +0200

    example search when db returned instance with rice seed_type

--- backend/search_by_seedtype/index.php
+++ backend/search_by_seedtype/index.php
@@ -1,4 +1,5 @@
 <?php
+//PHP endpoint for searching instances by entered seed type
 header('Content-Type: application/json; charset=utf-8');
 require_once "../PDOVxmlManager.php";
 
@@ -8,7 +9,9 @@ $userName = $config["username"];
 $userPassword = $config["password"];
 $databaseName = $config["dbname"];
 
-if (isset($_GET['seed_type'])) {
+$seed_type = "rice";
+
+if ($seed_type) { //isset($_GET['seed_type'])
     $pdoVxmlManager = new PDOVxmlManager($serverName, $userName, $userPassword, $databaseName);
     echo "PDO";
     $result = $pdoVxmlManager->getRecordingsBySeedType($_GET['seed_type']);
