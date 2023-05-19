71d33d3d66bab80bb89e6db74ae949d6e653e852
JezisVedToJeMojzis <mojzissamuel@gmail.com>
2023-05-19 12:19:55 +0200

    post method now saves recordings from vxml into local directory

--- backend/post_english_description/index.php
+++ backend/post_english_description/index.php
@@ -4,29 +4,33 @@
 //Description: This is a mobile-based program where users in Mali can call in and give or receive information on available seeds. The purpose of this program is to connect farmers to suppliers of seed as it can often be hard for them to establish such connections due to factors such as lack of internet and different mother tounges. -->
 
 //PHP endpoint for posting new english description of seed type into vxml table
-header('Content-Type: application/json; charset=utf-8');
-require_once "../PDODatabaseManager.php";
-
-$config = require_once "../config.php";
-$serverName = $config["host"];
-$userName = $config["username"];
-$userPassword = $config["password"];
-$databaseName = $config["dbname"];
+//header('Content-Type: application/json; charset=utf-8');
+//require_once "../PDODatabaseManager.php";
+//
+//$config = require_once "../config.php";
+//$serverName = $config["host"];
+//$userName = $config["username"];
+//$userPassword = $config["password"];
+//$databaseName = $config["dbname"];
 
 //$description = "Again posted new description about rice in english";
 //$seed_type = "rice";
 
 if (isset($_GET['seed_type'], $_GET['description'])) { //check if we received the needed values
-    $pdo = new PDODatabaseManager($serverName, $userName, $userPassword, $databaseName); //connect to db
-    $result = $pdo->postEnglishDescription($_GET['seed_type'], $_GET['description']); //send them to PDO manager to work with db
-    if ($result != null) {
-        header("HTTP/1.1 200 OK");
-        //echo json_encode($result);
-    } else {
-        header("HTTP/1.1 400 Bad Request");
-        echo "{} 400";
-    }
-}else {
-    header("HTTP/1.1 400 Bad Request");
-    echo "{}";
+    $seedType = $_GET['seed_type'];
+    $directory = '../../recordings/english/' + $seedType; // save recording to this directory
+    $filename = uniqid('voice_') . '.wav';
+    $filepath = $directory . $filename;
+
+    $description = $_POST['description'];
+
+    // decode and save the voice recording
+    $decodedData = base64_decode($description);
+    file_put_contents($filepath, $decodedData);
+
+    header("HTTP/1.1 200 OK");
+    echo "Voice recording saved successfully";
+} else {
+    header("HTTP/1.1 204 No content");
+    echo "Error, voice recording not found";
 }
